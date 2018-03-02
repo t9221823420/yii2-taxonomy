5b86485190624a917a2d13dec241f6a1646f30c0
Ivan Kryazhev <t9221823420@gmail.com>
2018-03-02 23:39:33 +0500

    daily

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -11,26 +11,11 @@ use yozh\taxonomy\AssetsBundle;
 class DefaultController extends Controller
 {
 	
-	public function behaviors()
-	{
-		return [
-			'access' => [
-				'class' => AccessControl::className(),
-				'rules' => [
-					[
-						'allow' => true,
-						'roles' => [ '@' ],
-					],
-				],
-			],
-		];
-	}
-	
 	public function actionIndex()
 	{
 		AssetsBundle::register( $this->view );
-		return $this->render( 'index', [
-		] );
+		
+		return $this->render( 'index', [] );
 	}
 	
 }
